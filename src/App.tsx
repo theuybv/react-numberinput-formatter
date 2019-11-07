b0a00dd61106af63dd2807ff16e220088a57ea79
Rense Bakker <b.rense@gmail.com>
2019-11-07 14:29:05 +0100

    more improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,77 @@
 import React, { useCallback, ChangeEvent, FormEvent, useState, useEffect } from 'react';
-import { Box, TextField } from '@material-ui/core';
+import { Box, TextField, InputAdornment } from '@material-ui/core';
 import { TextFieldProps } from '@material-ui/core/TextField';
-import numeral from 'numeral';
-import 'numeral/locales/nl-nl.js';
 
-numeral.locale('nl-nl');
+let formatter: Intl.NumberFormat, thousandSeparatorRegex: RegExp, decimalSeparatorRegex: RegExp, decimalSeparator: string;
 
-const NumericInput: React.FC<any> = ({ inputRef, onChange, onBlur, onFocus, decimalScale, showThousandSeparator, format, ...props }) => {
-  const [value, setValue] = useState(props.value || '');
+export const setLocale = (locale: any) => {
+  formatter = new Intl.NumberFormat(locale, { maximumFractionDigits: 1, useGrouping: true });
+  const parts = formatter.formatToParts(1234.5);
+  decimalSeparator = parts[3].value;
+  thousandSeparatorRegex = new RegExp('\\' + parts[1].value, 'g');
+  decimalSeparatorRegex = new RegExp('\\' + decimalSeparator, 'g');
+}
+
+setLocale('nl');
+
+export function format(value: number, options?: Intl.NumberFormatOptions) {
+  const { locale, ...oldOptions } = formatter.resolvedOptions();
+  const maximumFractionDigits = (options && options.maximumFractionDigits) ? options.maximumFractionDigits : 20;
+  return new Intl.NumberFormat(locale, { ...oldOptions, ...options, maximumFractionDigits }).format(value);
+}
+
+function toFloat(value: string) {
+  return parseFloat(value.replace(thousandSeparatorRegex, '').replace(decimalSeparatorRegex, '.'));
+}
+
+function testValidNumber(value: string, decimalScale?: number) {
+  return new RegExp('^-?\\d*[' + decimalSeparator + ']?\\d' + (decimalScale ? '{0,' + decimalScale + '}' : '*') + '$').test(value);
+}
+
+const NumericInput: React.FC<any> = ({ inputRef, onChange, onBlur, onFocus, maximumFractionDigits, useGrouping, ...props }) => {
+  const [value, setValue] = useState(props.value ? format(props.value, { useGrouping, maximumFractionDigits }) : '');
 
   useEffect(() => {
-    props.value !== '' && typeof props.value !== 'undefined' && setValue(numeral(props.value).format('0.[' + '0'.repeat(decimalScale) + ']'));
-  }, [props.value, decimalScale]);
+    if (props.value !== '' && typeof props.value !== 'undefined') {
+      setValue(format(props.value, { useGrouping, maximumFractionDigits }));
+    }
+  }, [props.value, useGrouping, maximumFractionDigits]);
 
   const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    const regex = new RegExp('^-?\\d*[,.]?\\d{0,' + decimalScale + '}$');
-    const isNumber = regex.test(e.target.value);
+    const isNumber = testValidNumber(e.target.value, maximumFractionDigits);
     if (isNumber || e.target.value === '') {
       setValue(e.target.value);
-      const newEvent = { ...e, target: { ...e.target, floatValue: isNumber ? numeral(e.target.value).value() : undefined } };
+      const numberValue = toFloat(e.target.value);
+      const newEvent = { ...e, target: { ...e.target, value: !isNaN(numberValue) ? numberValue : undefined } };
       onChange(newEvent);
     }
-  }, [onChange, decimalScale]);
+  }, [onChange, maximumFractionDigits]);
 
   const handleBlur = useCallback((e: FormEvent<HTMLInputElement>) => {
-    value !== '' && setValue(numeral(value).format((showThousandSeparator ? '0,' : '') + '0.[' + '0'.repeat(decimalScale) + ']'));
+    value !== '' && setValue(format(toFloat(value), { useGrouping, maximumFractionDigits }));
     onBlur(e);
-  }, [onBlur, value, decimalScale, showThousandSeparator]);
+  }, [onBlur, value, useGrouping, maximumFractionDigits]);
 
   const handleFocus = useCallback((e: FormEvent<HTMLInputElement>) => {
-    value !== '' && setValue(numeral(value).format('0.[' + '0'.repeat(decimalScale) + ']'));
+    value !== '' && useGrouping && setValue(format(toFloat(value), { useGrouping: false, maximumFractionDigits }));
     onFocus(e);
-  }, [onFocus, value, decimalScale]);
+  }, [onFocus, value, useGrouping, maximumFractionDigits]);
 
   return <input {...props} value={value || ''} onChange={handleChange} onBlur={handleBlur} onFocus={handleFocus} ref={inputRef} />
 };
 
 NumericInput.defaultProps = {
-  decimalScale: 2,
-  showThousandSeparator: false
+  useGrouping: false
 }
 
-const NumericTextField: React.FC<Omit<TextFieldProps, 'variant' | 'onChange' | 'value'> & { decimalScale?: number, showThousandSeparator?: boolean, value?: number, onChange?: (e: ChangeEvent<HTMLInputElement> & { target: { value: string, floatValue?: number } }) => void }> = ({ decimalScale, showThousandSeparator, InputProps, ...props }) => {
+type NumericTextFieldProps = Omit<TextFieldProps, 'variant' | 'onChange' | 'value'> & {
+  maximumFractionDigits?: number,
+  useGrouping?: boolean,
+  value?: number | string,
+  onChange?: (e: ChangeEvent<HTMLInputElement> & { target: { value?: number } }) => void
+}
+
+const NumericTextField: React.FC<NumericTextFieldProps> = ({ maximumFractionDigits, useGrouping, InputProps, ...props }) => {
   return (
     <TextField
       variant="outlined"
@@ -51,8 +81,8 @@ const NumericTextField: React.FC<Omit<TextFieldProps, 'variant' | 'onChange' | '
         ...InputProps,
         inputComponent: NumericInput,
         inputProps: {
-          decimalScale,
-          showThousandSeparator,
+          maximumFractionDigits,
+          useGrouping,
           pattern: '\\d*',
           ...InputProps ? InputProps.inputProps : {}
         }
@@ -61,14 +91,34 @@ const NumericTextField: React.FC<Omit<TextFieldProps, 'variant' | 'onChange' | '
   )
 }
 
+type CurrencyTextFieldProps = Omit<NumericTextFieldProps, 'maximumFractionDigits' | 'useGrouping'>
+
+const CurrencyTextField: React.FC<CurrencyTextFieldProps> = ({ InputProps, ...props }) => {
+  return (
+    <NumericTextField
+      {...props}
+      useGrouping={true}
+      maximumFractionDigits={2}
+      InputProps={{
+        startAdornment: <InputAdornment position="start">&euro;</InputAdornment>,
+        ...InputProps
+      }}
+    />
+  )
+}
+
 const App: React.FC = () => {
+  const [value, setValue] = useState(10);
   return (
     <Box height="100%" width="100%" justifyContent="center" alignItems="center" display="flex" flexDirection="column">
       <NumericTextField
         label="Nummers!"
-        onChange={e => console.log(e.target.floatValue)}
-        decimalScale={3}
-        showThousandSeparator={true}
+        value={value || ''}
+        onChange={e => setValue(e.target.value)}
+      />
+      <CurrencyTextField
+        label="Bedrag!"
+        onChange={e => console.log(e.target.value)}
       />
     </Box>
   );
