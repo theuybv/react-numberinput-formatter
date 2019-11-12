3485ce2d98f7f090d18f47497cc779782a172c60
Rense Bakker <b.rense@gmail.com>
2019-11-12 10:30:40 +0100

    demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import NumericInput, { setLocale, NumericProps} from 'react-numberinput-formatter/src';
+import { TextField, InputAdornment, Box } from '@material-ui/core';
+import { TextFieldProps } from '@material-ui/core/TextField';
+
+setLocale('nl');
+
+type NumericTextFieldProps = Omit<TextFieldProps, 'variant' | 'onChange' | 'value'> & NumericProps
+
+const NumericTextField: React.FC<NumericTextFieldProps> = ({ maximumFractionDigits, minimumFractionDigits, useGrouping, InputProps, ...props }) => {
+  return (
+    <TextField
+      variant="outlined"
+      margin="normal"
+      {...props}
+      InputProps={{
+        ...InputProps,
+        inputComponent: NumericInput as any,
+        inputProps: {
+          maximumFractionDigits,
+          minimumFractionDigits,
+          useGrouping,
+          pattern: '\\d*',
+          ...InputProps ? InputProps.inputProps : {}
+        }
+      }}
+    />
+  )
+}
+
+type CurrencyTextFieldProps = Omit<NumericTextFieldProps, 'maximumFractionDigits' | 'minimumFractionDigits' | 'useGrouping'>
+
+const CurrencyTextField: React.FC<CurrencyTextFieldProps> = ({ InputProps, ...props }) => {
+  return (
+    <NumericTextField
+      {...props}
+      useGrouping={true}
+      maximumFractionDigits={2}
+      minimumFractionDigits={2}
+      InputProps={{
+        startAdornment: <InputAdornment position="start">&euro;</InputAdornment>,
+        ...InputProps
+      }}
+    />
+  )
+}
 
 const App: React.FC = () => {
+  const [value, setValue] = useState(10 as number | undefined);
   return (
-    <span>test</span>
+    <Box height="100%" width="100%" justifyContent="center" alignItems="center" display="flex" flexDirection="column">
+      <NumericTextField
+        label="Numbers"
+        value={value || ''}
+        onChange={e => setValue(e.target.value)}
+      />
+      <CurrencyTextField
+        label="Currency"
+        onChange={e => console.log(e.target.value)}
+      />
+    </Box>
   );
 }
 
